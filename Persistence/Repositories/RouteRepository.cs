f8a25e17670c7ec1556656a6b89db439f78170b5
CharsDarwin <darwin.chamaya@unas.edu.pe>
2026-02-07 22:01:38 -0500

    antes de bajar

---
+++ Persistence/Repositories/RouteRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Persistence.Repositories
+{
+    internal class RouteRepository
+    {
+        public int IdRoute { get; set; }
+
+        public string Origin { get; set; } = null!;
+
+        public string Destination { get; set; } = null!;
+
+        public decimal DistanceKm { get; set; }
+
+        public TimeSpan EstimatedDuration { get; set; }
+
+        public decimal BasePrice { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+    }
+}
